db0d94747fc91ad5114af325958609e83411ba12
Petr Janda <petrjandacz@gmail.com>
2015-09-13 11:35:52 +0100

    different predictors

--- src/main/scala-2.10/job/Predict.scala
+++ src/main/scala-2.10/job/Predict.scala
@@ -1,13 +1,15 @@
 package job
 
-import org.apache.spark.ml.classification.GBTClassifier
-import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.classification.{RandomForestClassifier, MultilayerPerceptronClassifier, GBTClassifier}
+import org.apache.spark.ml.evaluation.{BinaryClassificationEvaluator, MulticlassClassificationEvaluator}
 import org.apache.spark.ml.feature.{IndexToString, Normalizer, StringIndexer, VectorAssembler}
 import org.apache.spark.ml.{MyOneHotEncoder, Pipeline, PipelineStage}
 import org.apache.spark.mllib.linalg.{Vector => Vec}
 import org.apache.spark.mllib.regression.LabeledPoint
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.DataFrame
+
+import scala.util.control.NonFatal
 
 
 object Predict extends SpringLeaf {
@@ -29,25 +31,55 @@ object Predict extends SpringLeaf {
         }
       }
 
-      def fixSchema(df: DataFrame, others: Set[String]): DataFrame =
+      val format = new java.text.SimpleDateFormat("ddMMMyy:HH:mm:ss")
+
+      val toDays = udf[Double, String] { s =>
+        try {
+          format.parse(s).getTime / (1000 * 60 * 60 * 24)
+        } catch {
+          case e:java.text.ParseException => 0.0
+        }
+      }
+
+      def fixStrings(df: DataFrame, others: Set[String]): DataFrame =
         others.foldLeft(df) { case (df, c) => df.withColumn(c, toDouble(df(c))) }
 
-      val data = preprocess(fixSchema(df, others = other), categorical)
+      val dates = Set(
+        "VAR_0073", "VAR_0075", "VAR_0156", "VAR_0157", "VAR_0158",
+        "VAR_0159", "VAR_0166", "VAR_0167", "VAR_0168", "VAR_0176",
+        "VAR_0177", "VAR_0178", "VAR_0179", "VAR_0204", "VAR_0217"
+      )
+
+      def fixDates(df: DataFrame, others: Set[String]): DataFrame =
+        others.foldLeft(df) { case (df, c) => df.withColumn(c, toDays(df(c))) }
+
+      val data = preprocess(
+        fixDates(fixStrings(df, others = other), others = dates), categorical)
         .select("indexedTarget", "features")
 
       val Array(trainingData, testData) = data.randomSplit(Array(0.7, 0.3)).map(_.cache())
 
-      testData.take(5).foreach(println)
+      val size = testData.select("features").take(1)(0).getAs[Vec]("features").size
 
-      val c = new GBTClassifier()
+
+      val c = new RandomForestClassifier()
         .setFeaturesCol("features")
         .setLabelCol("indexedTarget")
-        .setMaxIter(20)
+        .setMaxDepth(20)
 
-      val m = new MulticlassClassificationEvaluator()
+//      val c = new MultilayerPerceptronClassifier()
+//        .setFeaturesCol("features")
+//        .setLabelCol("indexedTarget")
+//        .setMaxIter(500)
+//        .setTol(1E-5)
+//        .setLayers(Array(size, 100, 2))
+
+      val m = new BinaryClassificationEvaluator()
         .setLabelCol("indexedTarget")
-        .setPredictionCol("prediction")
+        .setRawPredictionCol("probability")
+        .setMetricName("areaUnderROC")
 
+      println(m.evaluate(c.fit(trainingData).transform(trainingData)))
       println(m.evaluate(c.fit(trainingData).transform(testData)))
 
 
